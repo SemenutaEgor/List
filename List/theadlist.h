e79e97cf8f2d40971eea64e7f6ddc39a75b588ef
SemenutaEgor <e.semenuta@yandex.ru>
2020-03-19 15:06:59 +0300

    theadlist fixed

--- List/theadlist.h
+++ List/theadlist.h
@@ -1,10 +1,13 @@
-#pragma once
+//theadlist.h
+#ifndef _theadlist_h
+#define _theadlist_h
 #include "tlist.h"
 template <class T>
 class THeadList : public TList<T> {
 protected:
 	TNode<T> *pHead;
 public:
+
 	//constructor
 	THeadList() : TList<T>() {
 		pHead = new TNode<T>;
@@ -12,27 +15,33 @@ public:
 		pHead->pNext = pHead;
 		pFirst = pHead;
 	}
+
 	//destructor
 	~THeadList() {
 		TList<T> ::DelList();
 		delete pHead;
 	}
+
 	//add first link
 	void InsFirst(T el) {
 		TList::InsFirst(el);
 		pHead->pNext = pFirst;
 	}
+
 	// add last link
 	void InsLast(T el) {
 		TList::InsLast(el);
 	}
+
 	//add current link
 	void InsCurr(T el) {
 		TList::InsCurr(el);
 	}
+
 	// delete first link
 	void DelFirst() {
 		TList::DelFirst();
 		pHead->pNext = pFirst;
 	}
 };
+#endif
